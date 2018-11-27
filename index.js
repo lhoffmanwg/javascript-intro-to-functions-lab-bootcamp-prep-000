5d62816ae3afe9102530a61d124a590667a89b0d
Laura Hoffman <lhoffmanwg@gmail.com>
2018-11-27 05:47:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ function sayHiToGrandma(string) {
   if (string === whisper(string)) {
     return "I can't hear you!";
   } else if (string === shout(string)) {
-    return "YES INDEED!";
+      return "YES INDEED!";
   } else {
-    return "I love you, too.";
+      return "I love you, too.";
   }
 }
